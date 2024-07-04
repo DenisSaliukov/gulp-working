06d3a047fbe75b78ccea0be467a5bf443198a4e7
DenisSaliukov <den_jonatan@mail.ru>
2024-07-04 21:22:23 +0700

    Add files via upload

---
+++ ftp.js
@@ -0,0 +1,10 @@
+// Конфигурация FTP соединения
+export let configFTP = {
+	host: "stocks01.ftp.tools",
+	user: "stocks01_a2021",
+	password: "h6tG9dP9J7bP",
+	parallel: 10
+}
+// Путь к папке.
+// (!) Gulp добавит имя проекта (корневой папки) автоматически
+export let pathFTP = `/ivankozak/practice_2`;
